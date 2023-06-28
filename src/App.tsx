7a4dbcda2f54733d4f433835af9f3d2f74eb8890
Igor Shargin <igor20513@gmail.com>
2023-06-28 21:39:34 +0500

    feat: add style for button component

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,14 @@
+import { Button } from './components/ui/button'
+import { Typography } from './components/ui/typography/typography.tsx'
+
 export function App() {
-  return <div>Hello</div>
+  return (
+    <div>
+      <Typography variant={'large'}>Hello</Typography>
+      <Button>Primary</Button>
+      <Button variant={'secondary'}>Secondary</Button>
+      <Button variant={'link'}>Link</Button>
+      <Button variant={'tertiary'}>Tertiary</Button>
+    </div>
+  )
 }
